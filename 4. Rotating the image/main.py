f55beca13fe7288a452d61334e3bdbd394938054
shubham-00 <srpatel980@gmail.com>
2020-09-17 11:34:03 +0530

    rotating an image

---
+++ 4. Rotating the image/main.py
@@ -0,0 +1,39 @@
+import cv2
+
+
+image = cv2.imread("original_image.jpg")
+
+
+# Rotating an image
+h, w = image.shape[:2]
+center = (w // 2, h // 2)  # from where we want to rotate
+angle = 45  # angle from which we want to rotate
+scale = 1  # scale with which we want to
+
+matrix = cv2.getRotationMatrix2D(center, angle, scale)
+
+rotated_image = cv2.warpAffine(image, matrix, (w, h))
+"""
+alpha = scale*cos(angle)
+beta = scale*cos(angle)
+
+x, y = current pixel of image
+
+matrix = [[alpha, beta,  (1 - alpha) * center * x - beta * center * y],
+          [-beta, alpha, beta * center * x + (1 - alpha) * center * y]]
+
+wrapaffine does fillowing for all pixel values
+output = _x, _y = matrix cross [x,
+                                y,
+                                1]
+
+here output is generated for all pixel values
+and after combining them we get the new rotated image
+"""
+
+
+# Displaying rotated image
+cv2.imshow("Rotated image", rotated_image)
+
+
+cv2.waitKey(0)
